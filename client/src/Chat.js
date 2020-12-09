58324c8129ade0f8448d75c918dc375d00321142
otidapson <oladapo.o@softcom.ng>
2020-12-09 11:54:56 +0100

    implements  custom hooks for subscription

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,53 +1,69 @@
 import React from "react";
-import {useQuery,useMutation,useSubscription} from '@apollo/react-hooks'
-import {messagesQuery,addMessageMutation,msgAddedSubscription} from './graphql/queries'
+import { useQuery, useMutation, useSubscription } from '@apollo/react-hooks'
+import { messagesQuery, addMessageMutation, msgAddedSubscription } from './graphql/queries'
 import MessageInput from "./MessageInput";
 import MessageList from "./MessageList";
-//onSubscriptionData in useSubscription equivalent as onCompleted in useQuery
-const Chat =({user})=>{
- 
-    const {data}=useQuery(messagesQuery)
-    const messages=data?data.messages:[]
+// onCompleted in useQuery when query is complete
+//onSubscriptionData in useSubscription when subcription is made (here response still null) and when subscription receives new data
 
-  useSubscription(msgAddedSubscription,{
-    onSubscriptionData:({client,subscriptionData})=>{
-      client.writeData({data:{
-        messages:messages.concat(subscriptionData.data.messageAdded)//make sure data has same structure 
-        //as the query used to retrieve that data
-      }})     
+
+const useChatMsg = () => {
+
+  const { data } = useQuery(messagesQuery)
+  const messages = data ? data.messages : []
+
+  useSubscription(msgAddedSubscription, {
+    onSubscriptionData: ({ client, subscriptionData }) => {
+      client.writeData({
+        data: {
+          messages: messages.concat(subscriptionData.data.messageAdded)//make sure data has same structure 
+          //as the query used to retrieve that data
+        }
+      })
     }
   })
   //What useQuery does behind the seen for us
-    //const [res, setRes] =useState({loading:false})
+  //const [res, setRes] =useState({loading:false})
   // React.useEffect(()=>{
   //client.query({query:messagesQuery}).then(({data})=>setResult({loading:false,data}))
   //  })
-  
+
   const [addMessage] = useMutation(addMessageMutation)//useMutation is not executed instantly like 
   //useQuery but returns a tuple with first value as the fxn to call for the mutation in this case 'addMessage'
 
-  
+  return {
+    messages,
+    addMessage
+  }
+}
 
-  const handleSend=async (text)=>{
-    const {data} = await addMessage({ 
-      variables: { 
-        input: { text } 
+
+const Chat = ({ user }) => {
+
+
+  const { messages, addMessage } = useChatMsg()
+
+
+  const handleSend = async (text) => {
+    await addMessage({
+      variables: {
+        input: { text }
       }
     });
   }
   //if (loading) return <h1>loading...</h1>
-   //if (error) return <h1>Error Occured!...</h1>
-  return(
+  //if (error) return <h1>Error Occured!...</h1>
+  return (
     <section className='section'>
-          <div className='container'>
-            <h1 className='title'>Chatting as {user}</h1>
-            <MessageList user={user} messages={messages} />
-            <MessageInput onSend={handleSend.bind(this)} />
-          </div>
-        </section>
+      <div className='container'>
+        <h1 className='title'>Chatting as {user}</h1>
+        <MessageList user={user} messages={messages} />
+        <MessageInput onSend={handleSend.bind(this)} />
+      </div>
+    </section>
   )
 }
-  
+
 export default Chat;
 
 
@@ -69,7 +85,7 @@ export default Chat;
 //     }
 //   })
 
-  
+
 //   const [addMessage] = useMutation(addMessageMutation)//useMutation is not executed instantly like 
 
 // const handleSend=async (text)=>{
@@ -91,5 +107,5 @@ export default Chat;
 //         </section>
 //   )
 // }
-  
+
 // export default Chat;
