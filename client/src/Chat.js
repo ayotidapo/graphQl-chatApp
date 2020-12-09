315f3db85d1eaf97845418489193c71b557ccf9f
otidapson <oladapo.o@softcom.ng>
2020-12-09 11:02:14 +0100

    update code to use apollo react hooks

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,64 +1,102 @@
-import React, { useState } from "react";
-import { addMessage, getMessages, onMessageAdded } from "./graphql/queries";
+import React from "react";
+import {useQuery,useMutation,useSubscription} from '@apollo/react-hooks'
+import {messagesQuery,addMessageMutation,msgAddedSubscription} from './graphql/queries'
+import { addMessage } from "./graphql/queries";
 import MessageInput from "./MessageInput";
 import MessageList from "./MessageList";
-
+//onSubscriptionData in useSubscription equivalent as onCompleted in useQuery
 const Chat =({user})=>{
-const [messages,setMessages]=useState([])
-React.useEffect(()=>{
-  const messages = await getMessages();
-},[])
+ 
+    const {data}=useQuery(messagesQuery)
+    const messages=data?data.messages:[]
 
-const handleSend=()=>{
-   addMessage(text);
-}
-return(
-  <section className='section'>
-        <div className='container'>
-          <h1 className='title'>Chatting as {user}</h1>
-          <MessageList user={user} messages={messages} />
-          <MessageInput onSend={handleSend.bind(this)} />
-        </div>
-      </section>
-)
-}
-  state = { messages: [] };
-  subscription = null;
-
-  async componentDidMount() {
-    const messages = await getMessages();
-    this.setState({ messages });
-    this.subscription = onMessageAdded((message) => {
-
-      this.setState({ messages: this.state.messages.concat(message) });
-    });
-    
-  }
-
-  componentWillUnmount() {
-    /// this.subscription.unsubscribe();
-    if (this.subscription.unsubscribe) {
-      this.subscription.unsubscribe();
+  useSubscription(msgAddedSubscription,{
+    onSubscriptionData:({client,subscriptionData})=>{
+      client.writeData({data:{
+        messages:messages.concat(subscriptionData.data.messageAdded)//make sure data has same structure 
+        //as the query used to retrieve that data
+      }})     
     }
-  }
+  })
+  
+  
+  const [addMessage] = useMutation(addMessageMutation)//useMutation is not executed instantly like 
+  //useQuery but returns a tuple with first value as the fxn to call for the mutation in this case 'addMessage'
 
-  async handleSend(text) {
-    await addMessage(text);
-  }
+  
+    //What useQuery does behind the seen for us
+    //const [res, setRes] =useState({loading:false})
+  // React.useEffect(()=>{
+  //client.query({query:messagesQuery}).then(({data})=>setResult({loading:false,data}))
+  //  })
+    
+  // const [messages,setMessages]=useState([])
+  // React.useEffect(()=>{
+  //   const messages = await getMessages();
+  // },[])
 
-  render() {
-    const { user } = this.props;
-    const { messages } = this.state;
-    return (
-      <section className='section'>
-        <div className='container'>
-          <h1 className='title'>Chatting as {user}</h1>
-          <MessageList user={user} messages={messages} />
-          <MessageInput onSend={this.handleSend.bind(this)} />
-        </div>
-      </section>
-    );
+  const handleSend=async (text)=>{
+    const {data} = await addMessage({ 
+      variables: { 
+        input: { text } 
+      }
+    });
   }
+  //if (loading) return <h1>loading...</h1>
+   //if (error) return <h1>Error Occured!...</h1>
+  return(
+    <section className='section'>
+          <div className='container'>
+            <h1 className='title'>Chatting as {user}</h1>
+            <MessageList user={user} messages={messages} />
+            <MessageInput onSend={handleSend.bind(this)} />
+          </div>
+        </section>
+  )
 }
-
+  
 export default Chat;
+
+
+
+
+
+//Naive
+
+// const Chat =({user})=>{
+//   const [messages,setMessages]=React.useState([])
+//     useQuery(messagesQuery,{
+//       onCompleted:({messages})=>{
+//         setMessages(messages)
+//       }
+//     })
+//   useSubscription(msgAddedSubscription,{
+//     onSubscriptionData:({subscriptionData})=>{
+//       setMessages(messages.concat(subscriptionData.data.messageAdded))
+//     }
+//   })
+
+  
+//   const [addMessage] = useMutation(addMessageMutation)//useMutation is not executed instantly like 
+
+// const handleSend=async (text)=>{
+//   const {data} = await addMessage({ 
+//     variables: { 
+//       input: { text } 
+//     }
+//   });
+// }
+
+
+//   return(
+//     <section className='section'>
+//           <div className='container'>
+//             <h1 className='title'>Chatting as {user}</h1>
+//             <MessageList user={user} messages={messages} />
+//             <MessageInput onSend={handleSend.bind(this)} />
+//           </div>
+//         </section>
+//   )
+// }
+  
+// export default Chat;
