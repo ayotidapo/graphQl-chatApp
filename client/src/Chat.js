380a8ab52fee8500b2541e955b2f87cebe328a5f
otidapson <oladapo.o@softcom.ng>
2020-12-09 11:02:38 +0100

    update code to use apollo react hooks

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {useQuery,useMutation,useSubscription} from '@apollo/react-hooks'
 import {messagesQuery,addMessageMutation,msgAddedSubscription} from './graphql/queries'
-import { addMessage } from "./graphql/queries";
 import MessageInput from "./MessageInput";
 import MessageList from "./MessageList";
 //onSubscriptionData in useSubscription equivalent as onCompleted in useQuery
@@ -18,22 +17,16 @@ const Chat =({user})=>{
       }})     
     }
   })
-  
+  //What useQuery does behind the seen for us
+    //const [res, setRes] =useState({loading:false})
+  // React.useEffect(()=>{
+  //client.query({query:messagesQuery}).then(({data})=>setResult({loading:false,data}))
+  //  })
   
   const [addMessage] = useMutation(addMessageMutation)//useMutation is not executed instantly like 
   //useQuery but returns a tuple with first value as the fxn to call for the mutation in this case 'addMessage'
 
   
-    //What useQuery does behind the seen for us
-    //const [res, setRes] =useState({loading:false})
-  // React.useEffect(()=>{
-  //client.query({query:messagesQuery}).then(({data})=>setResult({loading:false,data}))
-  //  })
-    
-  // const [messages,setMessages]=useState([])
-  // React.useEffect(()=>{
-  //   const messages = await getMessages();
-  // },[])
 
   const handleSend=async (text)=>{
     const {data} = await addMessage({ 
